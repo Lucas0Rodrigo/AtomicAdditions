fb56180c45912f77aa9dee5f81da2244752241e8
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-08-23 16:21:18 -0300

    Oi

--- src/main/java/com/lucas/atomicadditions/AtomicCasingBlock.java
+++ src/main/java/com/lucas/atomicadditions/AtomicCasingBlock.java
@@ -1,14 +1,20 @@
 package com.lucas.atomicadditions;
 
-import net.minecraft.world.level.block.Block;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class AtomicCasingBlock extends Block {
+public class AtomicCasingBlock extends BaseEntityBlock {
 
     public AtomicCasingBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new AtomicCasingBlockEntity(pos, state);
     }
 }
