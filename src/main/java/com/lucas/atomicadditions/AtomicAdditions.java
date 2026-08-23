225117faeafc2151ec1765d8b13e73ec2472dc7d
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-08-23 16:21:18 -0300

    Oi

--- src/main/java/com/lucas/atomicadditions/AtomicAdditions.java
+++ src/main/java/com/lucas/atomicadditions/AtomicAdditions.java
@@ -68,7 +68,7 @@ public class AtomicAdditions
     // Casing de entrada
     public static final RegistryObject<Block> CASING_ENTRADA =
             BLOCKS.register("casing_entrada", () ->
-                    new Block(BlockBehaviour.Properties.of()
+                    new AtomicCasingBlock(BlockBehaviour.Properties.of()
                             .mapColor(MapColor.METAL)));
 
     // Casing de saída
