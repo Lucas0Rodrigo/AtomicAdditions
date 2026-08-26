05972b1d235e8119cb8aa790103e784b034039bf
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-08-26 13:08:49 -0300

    Mod em Produção.

--- src/main/java/com/lucas/atomicadditions/AtomicCasingBlockEntity.java
+++ src/main/java/com/lucas/atomicadditions/AtomicCasingBlockEntity.java
@@ -4,7 +4,6 @@ import mekanism.api.providers.IBlockProvider;
 import mekanism.common.lib.multiblock.MultiblockManager;
 import mekanism.common.tile.prefab.TileEntityMultiblock;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class AtomicCasingBlockEntity
@@ -41,45 +40,5 @@ public class AtomicCasingBlockEntity
             AtomicMultiblockData multiblock
     ) {
         super.structureChanged(multiblock);
-
-        if (level == null || level.isClientSide) {
-            return;
-        }
-
-        if (multiblock.isFormed()) {
-            level.players().stream()
-                    .filter(player ->
-                            player.distanceToSqr(
-                                    worldPosition.getX() + 0.5,
-                                    worldPosition.getY() + 0.5,
-                                    worldPosition.getZ() + 0.5
-                            ) <= 64 * 64
-                    )
-                    .forEach(player ->
-                            player.displayClientMessage(
-                                    Component.literal(
-                                            "Atomic Additions: Estrutura formada!"
-                                    ),
-                                    false
-                            )
-                    );
-        } else {
-            level.players().stream()
-                    .filter(player ->
-                            player.distanceToSqr(
-                                    worldPosition.getX() + 0.5,
-                                    worldPosition.getY() + 0.5,
-                                    worldPosition.getZ() + 0.5
-                            ) <= 64 * 64
-                    )
-                    .forEach(player ->
-                            player.displayClientMessage(
-                                    Component.literal(
-                                            "Atomic Additions: Estrutura desfeita!"
-                                    ),
-                                    false
-                            )
-                    );
-        }
     }
 }
