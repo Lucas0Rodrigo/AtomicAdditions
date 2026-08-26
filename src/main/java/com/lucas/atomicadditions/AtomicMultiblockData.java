439e86f9e2921f9536320bd05f5f8be6a1938f1a
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-08-26 14:58:17 -0300

    Mod em Produção.

--- src/main/java/com/lucas/atomicadditions/AtomicMultiblockData.java
+++ src/main/java/com/lucas/atomicadditions/AtomicMultiblockData.java
@@ -1,18 +1,51 @@
 package com.lucas.atomicadditions;
 
-import mekanism.common.lib.multiblock.MultiblockData;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.BlockEntity;
-
 import java.util.HashSet;
 import java.util.Set;
+import mekanism.common.capabilities.chemical.multiblock.MultiblockChemicalTankBuilder;
+import mekanism.common.lib.multiblock.MultiblockData;
+import mekanism.api.chemical.gas.IGasTank;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 public class AtomicMultiblockData extends MultiblockData {
 
     public final Set<BlockPos> coils = new HashSet<>();
 
+    public final IGasTank inputTank1;
+    public final IGasTank inputTank2;
+    public final IGasTank outputTank;
+
     public AtomicMultiblockData(BlockEntity tile) {
         super(tile);
+
+        inputTank1 = MultiblockChemicalTankBuilder.GAS.input(
+                this,
+                () -> 1_000_000L,
+                gas -> true,
+                mekanism.api.chemical.attribute.ChemicalAttributeValidator.ALWAYS_ALLOW,
+                createSaveAndComparator()
+        );
+
+        inputTank2 = MultiblockChemicalTankBuilder.GAS.input(
+                this,
+                () -> 1_000_000L,
+                gas -> true,
+                mekanism.api.chemical.attribute.ChemicalAttributeValidator.ALWAYS_ALLOW,
+                createSaveAndComparator()
+        );
+
+        outputTank = MultiblockChemicalTankBuilder.GAS.output(
+                this,
+                () -> 1_000_000L,
+                gas -> true,
+                mekanism.api.chemical.attribute.ChemicalAttributeValidator.ALWAYS_ALLOW,
+                this
+        );
+
+        gasTanks.add(inputTank1);
+        gasTanks.add(inputTank2);
+        gasTanks.add(outputTank);
     }
 
     public void addCoil(BlockPos pos) {
--- src/main/java/com/lucas/atomicadditions/AtomicScreen.java
+++ src/main/java/com/lucas/atomicadditions/AtomicScreen.java
@@ -1,7 +1,18 @@
 package com.lucas.atomicadditions;
 
+import java.util.ArrayList;
+import java.util.List;
 import mekanism.client.gui.GuiMekanismTile;
+import mekanism.client.gui.element.GuiInnerScreen;
+import mekanism.client.gui.element.bar.GuiBar.IBarInfoHandler;
+import mekanism.client.gui.element.bar.GuiDynamicHorizontalRateBar;
+import mekanism.client.gui.element.gauge.GaugeType;
+import mekanism.client.gui.element.gauge.GuiGasGauge;
+import mekanism.common.MekanismLang;
 import mekanism.common.inventory.container.tile.MekanismTileContainer;
+import mekanism.common.lib.Color;
+import mekanism.common.lib.Color.ColorFunction;
+import mekanism.common.util.text.TextUtils;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
@@ -17,11 +28,13 @@ public class AtomicScreen extends GuiMekanismTile<
             Inventory inv,
             Component title
     ) {
-        super(container, inv, Component.translatable("container.atomicadditions.atomic"));
+        super(
+                container,
+                inv,
+                Component.translatable("container.atomicadditions.atomic")
+        );
 
         dynamicSlots = true;
-
-        // Mesmo ajuste vertical utilizado pelo SPS.
         imageHeight += 16;
         inventoryLabelY = imageHeight - 92;
     }
@@ -29,6 +42,92 @@ public class AtomicScreen extends GuiMekanismTile<
     @Override
     protected void addGuiElements() {
         super.addGuiElements();
+
+        // Gás de entrada 1
+        addRenderableWidget(
+                new GuiGasGauge(
+                        () -> tile.getMultiblock().inputTank1,
+                        () -> tile.getMultiblock().getGasTanks(null),
+                        GaugeType.STANDARD,
+                        this,
+                        -11,
+                        17
+                )
+        );
+
+        // Gás de entrada 2
+        addRenderableWidget(
+                new GuiGasGauge(
+                        () -> tile.getMultiblock().inputTank2,
+                        () -> tile.getMultiblock().getGasTanks(null),
+                        GaugeType.STANDARD,
+                        this,
+                        7,
+                        17
+                )
+        );
+
+        // Gás de saída
+        addRenderableWidget(
+                new GuiGasGauge(
+                        () -> tile.getMultiblock().outputTank,
+                        () -> tile.getMultiblock().getGasTanks(null),
+                        GaugeType.STANDARD,
+                        this,
+                        151,
+                        17
+                )
+        );
+
+        // Painel de status
+        addRenderableWidget(
+                new GuiInnerScreen(
+                        this,
+                        27,
+                        17,
+                        122,
+                        60,
+                        () -> {
+                            List<Component> list = new ArrayList<>();
+
+                            list.add(
+                                    MekanismLang.STATUS.translate(
+                                            MekanismLang.IDLE
+                                    )
+                            );
+
+                            return list;
+                        }
+                )
+        );
+
+        // Barra de progresso
+        addRenderableWidget(
+                new GuiDynamicHorizontalRateBar(
+                        this,
+                        new IBarInfoHandler() {
+
+                            @Override
+                            public Component getTooltip() {
+                                return MekanismLang.PROGRESS.translate(
+                                        TextUtils.getPercent(0)
+                                );
+                            }
+
+                            @Override
+                            public double getLevel() {
+                                return 0;
+                            }
+                        },
+                        7,
+                        79,
+                        160,
+                        ColorFunction.scale(
+                                Color.rgbi(60, 45, 74),
+                                Color.rgbi(100, 30, 170)
+                        )
+                )
+        );
     }
 
     @Override
