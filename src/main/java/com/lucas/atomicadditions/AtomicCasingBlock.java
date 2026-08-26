a26e0465d005b78445a4465a71925fc4a37777c0
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-08-26 14:31:07 -0300

    Mod em Produção.

--- src/main/java/com/lucas/atomicadditions/AtomicCasingBlock.java
+++ src/main/java/com/lucas/atomicadditions/AtomicCasingBlock.java
@@ -6,6 +6,14 @@ import mekanism.common.block.interfaces.ITypeBlock;
 import mekanism.common.content.blocktype.BlockType;
 import mekanism.common.registration.impl.TileEntityTypeRegistryObject;
 import mekanism.common.tile.base.TileEntityMekanism;
+import mekanism.common.tile.base.WrenchResult;
+import mekanism.common.tile.prefab.TileEntityMultiblock;
+import mekanism.common.util.MekanismUtils;
+import mekanism.common.util.WorldUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
@@ -13,6 +21,8 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class AtomicCasingBlock<BE extends AtomicCasingBlockEntity>
@@ -81,5 +91,56 @@ public class AtomicCasingBlock<BE extends AtomicCasingBlockEntity>
                         blockState,
                         (TileEntityMekanism) blockEntity
                 );
+    }
+
+    @Override
+    @NotNull
+    @Deprecated
+    public InteractionResult use(
+            @NotNull BlockState state,
+            @NotNull Level level,
+            @NotNull BlockPos pos,
+            @NotNull Player player,
+            @NotNull InteractionHand hand,
+            @NotNull BlockHitResult hit
+    ) {
+        TileEntityMultiblock<?> tile =
+                WorldUtils.getTileEntity(
+                        TileEntityMultiblock.class,
+                        level,
+                        pos
+                );
+
+        if (tile == null) {
+            return InteractionResult.PASS;
+        }
+
+        if (level.isClientSide) {
+            if (!MekanismUtils.canUseAsWrench(
+                    player.getItemInHand(hand)
+            )) {
+                if (!tile.hasGui()
+                        || !tile.getMultiblock().isFormed()) {
+                    return InteractionResult.PASS;
+                }
+            }
+
+            return InteractionResult.SUCCESS;
+        }
+
+        if (tile.tryWrench(
+                state,
+                player,
+                hand,
+                hit
+        ) != WrenchResult.PASS) {
+            return InteractionResult.SUCCESS;
+        }
+
+        return tile.onActivate(
+                player,
+                hand,
+                player.getItemInHand(hand)
+        );
     }
 }
