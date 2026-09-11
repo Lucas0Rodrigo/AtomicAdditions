6dd29c2b57f587313c245234326fb047edee089f
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-09-10 21:03:08 -0300

    Correção do JEI tentativa 01

--- src/main/java/com/lucas/atomicadditions/integrations/jei/AtomicAdditionsJEIPlugin.java
+++ src/main/java/com/lucas/atomicadditions/integrations/jei/AtomicAdditionsJEIPlugin.java
@@ -1,15 +1,21 @@
 package com.lucas.atomicadditions.integrations.jei;
 
 import com.lucas.atomicadditions.AtomicAdditions;
+import com.lucas.atomicadditions.item.ActivatedCrystalItem;
 import com.lucas.atomicadditions.recipes.AtomicActivatedCrystalRecipe;
 import com.lucas.atomicadditions.recipes.AtomicRecipes;
 import mekanism.client.jei.MekanismJEI;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.UidContext;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 import java.util.List;
 
@@ -28,6 +34,39 @@ public class AtomicAdditionsJEIPlugin implements IModPlugin {
     }
 
     @Override
+    public void registerItemSubtypes(
+            ISubtypeRegistration registration
+    ) {
+        registration.registerSubtypeInterpreter(
+                AtomicAdditions.ACTIVATED_CRYSTAL.get(),
+                new IIngredientSubtypeInterpreter<ItemStack>() {
+
+                    @Override
+                    public String apply(
+                            ItemStack ingredient,
+                            UidContext context
+                    ) {
+                        CompoundTag tag =
+                                ingredient.getTag();
+
+                        if (tag == null ||
+                                !tag.contains(
+                                        ActivatedCrystalItem
+                                                .SOURCE_CRYSTAL_TAG
+                                )) {
+                            return IIngredientSubtypeInterpreter.NONE;
+                        }
+
+                        return tag.getString(
+                                ActivatedCrystalItem
+                                        .SOURCE_CRYSTAL_TAG
+                        );
+                    }
+                }
+        );
+    }
+
+    @Override
     public void registerCategories(
             IRecipeCategoryRegistration registration
     ) {
@@ -40,7 +79,6 @@ public class AtomicAdditionsJEIPlugin implements IModPlugin {
                 new AtomicAMRRecipeCategory(
                         guiHelper
                 ),
-
                 new AtomicActivatedCrystalRecipeCategory(
                         guiHelper
                 )
@@ -51,12 +89,6 @@ public class AtomicAdditionsJEIPlugin implements IModPlugin {
     public void registerRecipes(
             IRecipeRegistration registration
     ) {
-        /*
-         * =========================================================
-         * AMR
-         * =========================================================
-         */
-
         registration.addRecipes(
                 MekanismJEI.recipeType(
                         AtomicAMRRecipeCategory.TYPE
@@ -64,17 +96,6 @@ public class AtomicAdditionsJEIPlugin implements IModPlugin {
                 AtomicRecipes.AMR_RECIPES
                         .getRecipesForJEI()
         );
-
-        /*
-         * =========================================================
-         * ACTIVATED CRYSTAL
-         * =========================================================
-         *
-         * NÃO existe lista manual de cristais.
-         *
-         * Pegamos diretamente todas as receitas geradas
-         * dinamicamente pelo injector.
-         */
 
         List<AtomicActivatedCrystalRecipe>
                 activatedRecipes =
