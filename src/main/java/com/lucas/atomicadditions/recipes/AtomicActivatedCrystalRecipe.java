d0a9922cb2f6021084f8b4e694bfb76a4be7f6df
Lucas0Rodrigo <205671091+Lucas0Rodrigo@users.noreply.github.com>
2026-09-12 00:20:59 -0300

    Correção Recipe etapa 2 Tentativa 06 - OK

--- src/main/java/com/lucas/atomicadditions/recipes/AtomicActivatedCrystalRecipe.java
+++ src/main/java/com/lucas/atomicadditions/recipes/AtomicActivatedCrystalRecipe.java
@@ -107,11 +107,11 @@ public class AtomicActivatedCrystalRecipe extends ItemStackGasToItemStackRecipe 
     ) {
         ResourceLocation sourceId = BuiltInRegistries.ITEM.getKey(sourceCrystal.getItem());
 
-        ItemStack activatedInput = new ItemStack(AtomicAdditions.ACTIVATED_CRYSTAL.get(), 1);
+        ItemStack activatedInput = new ItemStack(AtomicAdditions.ACTIVATED_CRYSTAL.get(), 8);
         ActivatedCrystalItem.setSourceCrystal(activatedInput, sourceId);
 
         ItemStack output = shardOutput.copy();
-        output.setCount(2);
+        output.setCount(16);
 
         AtomicAdditions.LOGGER.info("[AA DEBUG] Criando receita RHENIUM para fonte: {} -> Output: {} x{}",
                 sourceId, BuiltInRegistries.ITEM.getKey(output.getItem()), output.getCount());
